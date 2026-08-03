41163b5b3189430d91071de807b88cadf7e6385a
GoldenApple <suyuqing-555@qq.com>
2026-08-04 00:46:25 +0800

    Develop (#16)
    * 2026.8.1-2
    
    * 2026.8.2-1
    
    * 2026.8.3-1
    
    * 2026.8.4-2
    
    * 2026.8.4-3

--- pages/minecraft_pages/function_page/menu/menu.js
+++ pages/minecraft_pages/function_page/menu/menu.js
@@ -33,6 +33,9 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
+    this.setData({
+      app: getApp()
+    });
     console.info("page_minecraft_function_menu:页面初次渲染完成\n");
   },
 
--- pages/more/more.js
+++ pages/more/more.js
@@ -12,21 +12,30 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.setData({
+      app: getApp()
+    });
+    console.info("page_more:页面加载完成\n", options);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
+    this.setData({
+      app: getApp()
+    });
+    console.info("page_more:页面初次渲染完成");
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    this.setData({
+      app: getApp()
+    });
+    console.info("page_mods:页面渲染");
   },
 
   /**
